13981158bdc657ab655819e7b66881b71703c517
Eric Giovanola <ericgio@gmail.com>
2019-03-18 11:21:46 -0700

    Fix failing reducer test

--- store/__tests__/rootReducer.test.js
+++ store/__tests__/rootReducer.test.js
@@ -1,15 +1,20 @@
 import rootReducer from '../reducers';
 
+const history = { location: {} }; // Mock the `history` object.
+const initialState = {};
+const action = { type: 'FOO' };
+
 describe('rootReducer', () => {
   // Basic test for overall app state.
   test('contains the expected subreducers', () => {
-    const state = rootReducer({}, { type: 'FOO' });
+    const state = rootReducer(history)(initialState, action);
 
     expect(Object.keys(state)).toEqual([
       'activities',
       'brands',
       'garminActivity',
       'pendingRequests',
+      'router',
       'searchResults',
       'session',
       'shoes',
