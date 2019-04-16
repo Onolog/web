5f9217d2c68bb49897aa92e77b9cceda70dec1b2
Eric Giovanola <ericgio@gmail.com>
2019-04-16 12:24:02 -0700

    Upgrade react-router-bootstrap and react-router-dom

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -11,22 +11,7 @@ const App = () => (
   <ErrorBoundary>
     <Switch>
       {routes.map((route: RouteType) => (
-        <Route
-          {...route}
-          key={route.path}
-          component={(routerProps, props) => {
-            // HACK: Explicitly handle the route component.
-            // @loadable/component uses `forwardRef` to wrap each
-            // component. However, react-router-dom < v4.4 doesn't handle
-            // `forwardRef` and warns that the component is an object instead of
-            // a function. Solution would be to upgrade RR, BUT RR >= v4.4 uses
-            // the new context API and breaks `react-router-bootstrap` :(
-            // When RRB releases a new version that accounts for context, we can
-            // then upgrade RR to v4.4 or v5 and get rid of this workaround.
-            const Component = route.component;
-            return <Component {...routerProps} {...props} />;
-          }}
-        />
+        <Route {...route} key={route.path} />
       ))}
     </Switch>
   </ErrorBoundary>
